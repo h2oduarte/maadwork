133e076573bb85e461ba3343718bc5c46ec24843
unknown <aguaduarte@hotmail.com>
2020-02-18 15:40:14 -0300

    remove comments

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+maad - 18022020
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
